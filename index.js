caf8c3f863a8b01205e44a7f1465d91769fe667b
Aletha Wheeless <augustkama98@gmail.com>
2022-05-20 18:23:54 -1000

    commit tree 16dcb0bd90a46b39afcd6f439acd70cf22cc7b5f

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+function submitData() {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify({
+          name: "Steve",
+          email: "steve@steve.com",
+        }),
+      })
+      .then(res => res.json())
+      .then(getID)
+      .catch(errorFn)
+
+}
+submitData()
+function getID(obj) {
+    document.body.innerHTML = obj.id
+}
+function errorFn(error) {
+    document.body.innerHTML = error.message
+}
